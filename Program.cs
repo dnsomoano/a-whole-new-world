d1c13e650c72703382fc14e94267a5793fc0c368
Daniel N Somoano <dnsomoano@gmail.com>
2018-09-10 23:43:14 -0400

    Code is not refactored whatsoever, but logic is present and complete

--- Program.cs
+++ Program.cs
@@ -6,32 +6,106 @@ namespace a_whole_new_world
     {
         static void Greeting()
         {
+            var userName = "";
             Console.WriteLine("Hello user! What shall I call you?");
-            var userName = Console.ReadLine();
-            System.Console.WriteLine($"Well {userName}.. I would like you to choose a number from 1 to 100:");
+            userName = Console.ReadLine();
+            System.Console.WriteLine($"Well {userName}..");
         }
 
-        static int AddGuess()
-        {
-            var userInput = int.Parse(Console.ReadLine());
-            System.Console.WriteLine($"This works, the number is {userInput}");
-            return userInput;
-        }
+        // function for guesses
+        // static void UpdateGame(double guess)
+        // {
+        //     double lower = 0.00;
+        //     double upper = 0.00;
+        //     var response = "";
+        //     lower = MIN;
+        //     upper = MAX;
+        //     guess = Math.Round((lower + upper) / 2, 0);
+        //     var tries = 0.00;
+        //     tries = Math.Round(Math.Log(MAX) / Math.Log(2));
+        //     // Output # of guesses
+        //     System.Console.WriteLine($"I want you think of a number {MIN} to {MAX}");
+        //     System.Console.WriteLine($"I can guess the number you chose in {tries} tries");
+        //     System.Console.WriteLine("What do you say to that?");
+        //     response = Console.ReadLine();
+        //     // output guess
+        //     System.Console.WriteLine($"I bet you do, anyway... Is your number {guess}?");
+        //     UserChoice(guess);
+        // }
 
-        static int CalculateNumber()
-        {
-            Random randomGenerator = new Random();
-            int randomNumber = randomGenerator.Next(1, 100);
-            System.Console.WriteLine(randomNumber);
-            return randomNumber;
-        }
-
+        // static void UserChoice(double guess)
+        // {
+        //     var userInput = 0;
+        //     // ask user for a lower/higher response
+        //     System.Console.WriteLine("Choose 1 for lower and 2 for upper");
+        //     userInput = int.Parse(Console.ReadLine());
+        //     if (userInput == 1)
+        //     {
+        //         GuessLower(guess);
+        //     }
+        //     else
+        //     {
+        //         GuessUpper(guess);
+        //     }
+        // }
+        // static void GuessUpper(double userGuess)
+        // {
+        //     double upperChoice = userGuess + 1;
+        //     UpdateGame(upperChoice);
+        // }
+        // static void GuessLower(double userGuess)
+        // {
+        //     double lowerChoice = userGuess - 1;
+        //     UpdateGame(lowerChoice);
+        // }
+        const double MIN = 1;
+        const double MAX = 100;
         static void Main(string[] args)
         {
+            var guess = 0.00;
+            var tries = 0.00;
+            double lower = 0.00;
+            double upper = 0.00;
+            var userInput = 0;
+            int response = 0;
             Greeting();
-            AddGuess();
-            // TODO pass user's guess to calculate random number and compare
-            CalculateNumber();
+            // UpdateGame(guess);
+            // Calculates # of guesses
+            lower = MIN;
+            upper = MAX;
+            guess = Math.Round((lower + upper) / 2, 0);
+            tries = Math.Round(Math.Log(MAX) / Math.Log(2));
+            // Output # of guesses
+            System.Console.WriteLine($"I want you think of a number {MIN} to {MAX}");
+            System.Console.WriteLine($"I can guess the number you chose in {tries} tries");
+            // output guess
+            System.Console.WriteLine($"I bet you do, anyway... Is your number {guess}?");
+            System.Console.WriteLine("What do you say to that? 1 for Yes, 2 for No");
+            response = int.Parse(Console.ReadLine());
+            while (response != 1)
+            {
+                // ask user for a lower/higher response
+                System.Console.WriteLine("Choose 1 for lower and 2 for upper");
+                userInput = int.Parse(Console.ReadLine());
+                if (userInput == 1)
+                {
+                    // GuessLower(guess);
+                    upper = guess - 1;
+                    guess = Math.Round((lower + upper) / 2, 0);
+                    System.Console.WriteLine($"Is your number {guess}?");
+                    tries--;
+                    System.Console.WriteLine($"Tries left are {tries}");
+                }
+                else
+                {
+                    // GuessUpper(guess);
+                    lower = guess + 1;
+                    guess = Math.Round((lower + upper) / 2, 0);
+                    System.Console.WriteLine($"Is your number {guess}?");
+                    tries--;
+                    System.Console.WriteLine($"Tries left are {tries}");
+                }
+            }
         }
     }
 }
